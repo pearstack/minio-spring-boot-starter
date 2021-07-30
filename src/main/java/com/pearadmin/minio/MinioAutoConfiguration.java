bca8deb5861d78683274c032ad682acfcb76af84
就眠仪式 <854085467@qq.com>
2021-07-30 17:09:28 +0800

    remove lombok

--- src/main/java/com/pearadmin/minio/MinioAutoConfiguration.java
+++ src/main/java/com/pearadmin/minio/MinioAutoConfiguration.java
@@ -3,7 +3,6 @@ package com.pearadmin.minio;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
